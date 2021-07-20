6403839d517347f420919f946d714bdbc808c17e
kuro4 <kuro4king@gmail.com>
2021-07-20 21:12:50 +0300

    create RegionControllerTest

--- src/test/java/com/vladimir1506/crud_hibernate/controller/RegionControllerTest.java
+++ src/test/java/com/vladimir1506/crud_hibernate/controller/RegionControllerTest.java
@@ -1,4 +1,4 @@
-package com.vladimir1506.crud_hibernate.repository.implementation;
+package com.vladimir1506.crud_hibernate.controller;
 
 import com.vladimir1506.crud_hibernate.model.Region;
 import org.junit.Before;
@@ -10,22 +10,18 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class DBRegionRepositoryImplTest {
-    DBRegionRepositoryImpl dbRegionRepository;
-    Region reg;
+public class RegionControllerTest {
+    RegionController regionController;
 
     @Before
     public void setUp() {
         List<Region> regions = new ArrayList<>();
-        reg = new Region("US");
-        reg.setId(1L);
-        regions.add(reg);
-        dbRegionRepository = mock(DBRegionRepositoryImpl.class);
-        when(dbRegionRepository.getAll()).thenReturn(regions);
-        when(dbRegionRepository.getById(1L)).thenReturn(reg);
-        Region updatedRegion = new Region("EU");
-        updatedRegion.setId(1L);
-        doReturn(updatedRegion).when(dbRegionRepository).update(reg);
+        Region region = new Region("US");
+        region.setId(1L);
+        regions.add(region);
+        regionController = mock(RegionController.class);
+        when(regionController.getAll()).thenReturn(regions);
+        when(regionController.getRegionById(1L)).thenReturn(region);
     }
 
     @Test
@@ -34,25 +30,25 @@ public class DBRegionRepositoryImplTest {
         Region region = new Region("US");
         region.setId(1L);
         regionsForTest.add(region);
-        List<Region> regions = dbRegionRepository.getAll();
+        List<Region> regions = regionController.getAll();
         assertEquals(regionsForTest.size(), regions.size());
         assertEquals(regionsForTest.get(0).getId(),
                 regions.get(0).getId());
         assertEquals(regionsForTest.get(0).getName(),
                 regions.get(0).getName());
-        verify(dbRegionRepository).getAll();
+        verify(regionController).getAll();
     }
 
     @Test
     public void getByIdTest() {
         Region region = new Region("US");
         region.setId(1L);
-        Region regionById = dbRegionRepository.getById(1L);
+        Region regionById = regionController.getRegionById(1L);
 
         assertEquals(region.getId(), regionById.getId());
         assertEquals(region.getName(), regionById.getName());
 
-        verify(dbRegionRepository, atLeastOnce()).getById(1L);
+        verify(regionController, atLeastOnce()).getRegionById(1L);
     }
 
     @Test
@@ -60,13 +56,8 @@ public class DBRegionRepositoryImplTest {
         Region region = new Region("US");
         region.setId(1L);
         region.setName("EU");
-        Region updatedRegion = dbRegionRepository.
-                update(reg);
-
-        assertEquals(region.getId(), updatedRegion.getId());
-        assertEquals(region.getName(), updatedRegion.getName());
-
-        verify(dbRegionRepository).update(reg);
+        regionController.updateRegion(region.getId(), region.getName());
+        verify(regionController).updateRegion(region.getId(), region.getName());
     }
 
 }
